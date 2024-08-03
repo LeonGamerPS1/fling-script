7bfb6448723104939499fea7bae4e20f17dbf62e
LeonGamerPS4 <108237023+LeonGamerPS1@users.noreply.github.com>
2024-08-03 21:34:49 +0200

    Update dontuse.hx

--- dontuse.hx
+++ dontuse.hx
@@ -1,12 +1,16 @@
 package;
 
 import fling.LuaUtil;
-
+import fling.LuaCode;
 
 class HaxeLibExecutor extends LuaUtil {
 
   override function onInject(spammed:Bool) {
     super.onInject(spammed);
   }
-  
+
+ override function execute(code:LuaCode) {
+   super.exeucute(code);
+   trace(code);
+ }
 }
